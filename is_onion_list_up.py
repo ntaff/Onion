ef9c6794d8f290a983fa97b641f7d27a6dc3bcf0
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2020-07-06 16:48:14 +0200

    Update is_onion_list_up.py

--- is_onion_list_up.py
+++ is_onion_list_up.py
@@ -1,25 +1,33 @@
 import requests
 import re
+from time import sleep
+import datetime
+#print(requests.get('http://verified2ebdpvms.onion/favicon.ico').status_code)
 
-# /!\ ACTIVATE KALITORIFY FIRST /!\
+# /!\ Activate Kalitorify first /!\
 
-fin = open("test.md", "r")
-content = fin.readlines()
-content = [x.strip() for x in content]
-fin.close()
-fin = open("test.md", "w+")
-for line in content:
-    print(line)
-    s = re.search('([a-zA-Z0-9.\/:[\]()-]*]*).\|.([a-zA-Z0-9.\/:[\]()-]*]*).\|.([a-zA-Z0-9.\/:[\]()!-]*]*)', line)
-    if s :
-        if ".onion" in s.groups()[1]:
-            ping = 99
-            if ping != 200 :
-    	           fin.write(line.replace(s.groups()[2], '![down](https://img.shields.io/badge/Statut-DOWN-red)')+'\n')
-            if ping == 200 :
-                   fin.write(line.replace(s.groups()[2], '![up](https://img.shields.io/badge/Statut-UP-green)')+'\n')
+
+statuscodes = (200,301,302,401,403,504)
+while(1):
+    fin = open("test.md", "r")
+    content = [x.strip() for x in fin.readlines()]
+    fin.close()
+    fin = open("test.md", "w+")
+    for line in content:
+        s = re.search('([a-zA-Z0-9.\/:[\]()-]*]*).\|.([a-zA-Z0-9.\/:[\]()-]*]*).\|.([a-zA-Z0-9.\/:[\]()!-]*]*)', line)
+        if s :
+            if ".onion" in s.groups()[1]:
+                try :
+                    if ping := requests.get(s.groups()[1], timeout=20, verify = False).status_code in statuscodes:
+                        fin.write(line.replace(s.groups()[2], '![up](https://img.shields.io/badge/Statut-UP-green)')+'\n')
+                    else :
+    	                fin.write(line.replace(s.groups()[2], '![down](https://img.shields.io/badge/Statut-DOWN-red)')+'\n')
+                except :
+                    fin.write(line.replace(s.groups()[2], '![down](https://img.shields.io/badge/Statut-DOWN-red)')+'\n')
+            else :
+                fin.write(line+'\n')
         else :
             fin.write(line+'\n')
-    else :
-        fin.write(line+'\n')
-fin.close()
+    fin.close()
+    print(f"Fichier actualisé le {str(datetime.datetime.now())}")
+    sleep(1800)
